947c9f11dd425369db79411d65d9cdf58e817a76
troubleshootist <turboshnek@yandex.ru>
2022-05-28 19:25:52 +0300

    new commit

--- training_center/training_center/settings.py
+++ training_center/training_center/settings.py
@@ -71,21 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'training_center.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     "default": {
-#     "ENGINE": "django.db.backends.postgresql_psycopg2",
-#     "NAME": "mcq_gen",
-#     "USER" : "dmitriy",
-#     "PASSWORD" : "12345",
-#     "HOST" : "127.0.0.1",
-#     "PORT" : "5432",
-
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
